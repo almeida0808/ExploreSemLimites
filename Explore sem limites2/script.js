522e4a611335577963ad5f3a4be28c80d588f656
almeida0808 <almeidaff08@gmail.com>
2023-11-09 12:03:38 -0300

    1

---
+++ Explore sem limites2/script.js
@@ -0,0 +1,9 @@
+window.sr = ScrollReveal({reset: true});
+sr.reveal('.card1' , {delay:300,  scale:1.2, duration:300, easing: 'ease-out',})
+sr.reveal('.card2', {delay:500,  scale:1.2, duration:300 , easing: 'ease-out',})
+sr.reveal('.card3' , {delay:700,  scale:1.2, duration:300 , easing: 'ease-out',} )
+
+sr.reveal('.contato' , {delay:200 , origin: 'left' , distance: '30px',duration: 600})
+sr.reveal('.bg-balls' , {delay:200 , origin: 'left' , distance: '3000px',duration: 600})
+
+sr.reveal('.footer' ,{ origin: 'bottom' ,  distance: '5px' })
